6e1f5000e1e29ff7057026a2e055d9363ccb3ed2
murilo-souza <murilo.l.souza232@gmail.com>
2021-09-23 00:10:57 -0300

    update

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head  from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Início | Ig.news</title> 
+      </Head>
+      <h1>
+        Hello word
+      </h1>
+    </>
+  )
+}
