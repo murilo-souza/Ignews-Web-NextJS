f48b208ed0cd3fa55d429e915398c56327f2c195
murilo-souza <murilo.l.souza232@gmail.com>
2021-09-28 17:48:44 -0300

    update

--- src/pages/api/subscribe.ts
+++ src/pages/api/subscribe.ts
@@ -1,17 +1,54 @@
 import {NextApiRequest, NextApiResponse} from 'next'
 import {stripe} from '../../services/stripe'
 import {getSession} from 'next-auth/client'
+import { fauna } from '../../services/fauna'
+import {query as q} from 'faunadb'
+
+type User = {
+    ref: {
+        id: string;
+    }
+    data: {
+        stripe_customer: string
+    }
+}
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-    if(req.method === 'POST'){
+    if(req.method == 'POST'){
         const session = await getSession({req})
         
-        const stripeCustomer = await stripe.customers.create({
-            email: session.user.email,
-        })
+        const user = await fauna.query<User>(
+            q.Get(
+                q.Match(
+                    q.Index('user_by_email'),
+                    q.Casefold(session.user.email)
+                )
+            )
+        )
 
+        let customerId = user.data.stripe_customer
+
+        if(!customerId){
+            const stripeCustomer = await stripe.customers.create({
+                email: session.user.email,
+            })
+    
+            await fauna.query(
+                q.Update(
+                    q.Ref(q.Collection('users'), user.ref.id),
+                    {
+                        data: {
+                            stripe_customer: stripeCustomer.id,
+                        }
+                    }
+                )
+            )
+            customerId = stripeCustomer.id
+        }
+
+        
         const stripeCheckoutSession = await stripe.checkout.sessions.create({
-            customer: stripeCustomer.id,
+            customer: customerId,
             payment_method_types: ['card'],
             billing_address_collection: 'required',
             line_items: [
@@ -20,7 +57,7 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
             mode: 'subscription',
             allow_promotion_codes: true,
             success_url: process.env.STRIPE_SUCCESS_URL,
-            cancel_url: process.env.STRIPE_CANCEL_URL
+            cancel_url: process.env.STRIPE_CANCEL_URL,
         })
 
         return res.status(200).json({sessionId: stripeCheckoutSession.id})
