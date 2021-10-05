9bd9a5785058c2ca7bbfb78047b0f54a334166f2
murilo-souza <murilo.l.souza232@gmail.com>
2021-10-05 16:03:42 -0300

    update

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,8 @@
 import { SignInButton } from '../SignInButton';
+import Link from 'next/link'
 import styles from './styles.module.scss';
+import { useRouter } from 'next/router';
+import { ActiveLink } from '../ActiveLink';
 
 export function Header(){
     return (
@@ -7,8 +10,12 @@ export function Header(){
             <div className={styles.headerContent}>
                 <img src="/images/logo.svg" alt="ig.news"/>
                 <nav>
-                    <a className={styles.active}>Home</a>
-                    <a>Posts</a>
+                    <ActiveLink activeClassName={styles.active} href="/">
+                        <a>Home</a>
+                    </ActiveLink>
+                    <ActiveLink activeClassName={styles.active} href="/posts">
+                        <a>Posts</a>
+                    </ActiveLink>
                 </nav>
                 <SignInButton/>
             </div>
